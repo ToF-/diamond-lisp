0148f85ec06e3013adae49f256a52660dc83a3bb
Christophe Thibaut <cthibauttof@gmail.com>
2024-03-22 19:11:40 +0100

    refactor eliminate duplication

--- src/diamond.lisp
+++ src/diamond.lisp
@@ -3,15 +3,13 @@
   (append xs (cdr (reverse xs))))
 
 (defun diamond (letter)
-  (mirror (cond ((equal 'A letter)
-                 '((#\A)))
-                ((equal 'B letter)
-                 (mapcar #'mirror
-                         '((#\SPACE #\A)
-                           (#\B #\SPACE))))
-                (T (mapcar #'mirror
-                           '((#\SPACE #\SPACE #\A)
-                             (#\SPACE #\B #\SPACE)
-                             (#\C #\SPACE #\SPACE)))))))
+  (mirror (mapcar #'mirror (cond ((equal 'A letter)
+                                  '((#\A)))
+                                 ((equal 'B letter)
+                                          '((#\SPACE #\A)
+                                            (#\B #\SPACE)))
+                                 (T '((#\SPACE #\SPACE #\A)
+                                      (#\SPACE #\B #\SPACE)
+                                      (#\C #\SPACE #\SPACE)))))))
 
 
