55bb0ef5a30870baca795505ba5bc9499446f793
Christophe Thibaut <cthibauttof@gmail.com>
2024-03-22 19:35:45 +0100

    refactor extract method pattern

--- src/diamond.lisp
+++ src/diamond.lisp
@@ -7,15 +7,18 @@
       ()
       (cons #\SPACE (spaces (1- n)))))
 
+(defun pattern (left letter right)
+    (append (spaces left) (list letter) (spaces right)))
+
 (defun diagonal (letter)
   (cond ((equal 'A letter)
          '((#\A)))
         ((equal 'B letter)
          '((#\SPACE #\A)
            (#\B #\SPACE)))
-        (T (list (append (spaces 2) (list #\A) (spaces 0))
-                 (append (spaces 1) (list #\B) (spaces 1))
-                 (append (spaces 0) (list #\C) (spaces 2))))))
+        (T (list (pattern 2 #\A 0)
+                 (pattern 1 #\B 1)
+                 (pattern 0 #\C 2)))))
 
 
 (defun diamond (letter)
