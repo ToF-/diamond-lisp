31aeae4e97025bf0fdc51406a3e97067a674b4f3
Christophe Thibaut <cthibauttof@gmail.com>
2024-03-22 19:24:05 +0100

    refactor develop using spaces

--- src/diamond.lisp
+++ src/diamond.lisp
@@ -13,9 +13,9 @@
         ((equal 'B letter)
          '((#\SPACE #\A)
            (#\B #\SPACE)))
-        (T (list (append (spaces 2) (list #\A))
-                 (list #\SPACE #\B #\SPACE)
-                 (list #\C #\SPACE #\SPACE)))))
+        (T (list (append (spaces 2) (list #\A) (spaces 0))
+                 (append (spaces 1) (list #\B) (spaces 1))
+                 (append (spaces 0) (list #\C) (spaces 2))))))
 
 
 (defun diamond (letter)
