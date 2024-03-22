ab17dd71e0b0f38c67078773b21dfa1154d82fe2
Christophe Thibaut <cthibauttof@gmail.com>
2024-03-22 18:57:41 +0100

    refactor extract method mirror

--- src/diamond.lisp
+++ src/diamond.lisp
@@ -1,15 +1,15 @@
 
+(defun mirror (lines)
+  (append lines (cdr (reverse lines))))
+
 (defun diamond (letter)
   (cond ((equal 'A letter)
-         '((#\A)))
+         (mirror '((#\A))))
         ((equal 'B letter)
-         '((#\SPACE #\A #\SPACE)
-           (#\B #\SPACE #\B)
-           (#\SPACE #\A #\SPACE)))
-        (T '((#\SPACE \#SPACE #\A #\SPACE #\SPACE)
-             (#\SPACE \#B #\SPACE #\B #\SPACE)
-             (#\C #\SPACE #\SPACE #\SPACE #\C)
-             (#\SPACE \#B #\SPACE #\B #\SPACE)
-             (#\SPACE \#SPACE #\A #\SPACE #\SPACE)))))
+         (mirror '((#\SPACE #\A #\SPACE)
+                   (#\B #\SPACE #\B))))
+        (T (mirror '((#\SPACE \#SPACE #\A #\SPACE #\SPACE)
+                     (#\SPACE \#B #\SPACE #\B #\SPACE)
+                     (#\C #\SPACE #\SPACE #\SPACE #\C))))))
 
 
