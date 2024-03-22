9478f4da926bc8070926e2684616c7f0d01e923e
ToF_ <cthibauttof@gmail.com>
2024-03-22 11:08:28 +0100

    diamond B is a 3 by 3 diamond A B B A

--- src/diamond.lisp
+++ src/diamond.lisp
@@ -1,3 +1,7 @@
 
 (defun diamond (letter)
-  '((#\A)))
+  (cond ((equal 'A letter)
+            '((#\A)))
+        (T '((#\SPACE #\A #\SPACE)
+            (#\B #\SPACE #\B)
+            (#\SPACE #\A #\SPACE)))))
--- test/diamond-test.lisp
+++ test/diamond-test.lisp
@@ -7,5 +7,11 @@
 (define-test smallest-diamond-is-A
     (assert-equal '((#\A)) (diamond 'A)))
 
+(define-test diamond-B-is-3-by-3-A-B-B-A
+    (assert-equal '((#\SPACE #\A #\SPACE)
+                    (#\B #\SPACE #\B)
+                    (#\SPACE #\A #\SPACE))
+                  (diamond 'B)))
+
 (run-tests :all)
 (sb-ext:quit)
